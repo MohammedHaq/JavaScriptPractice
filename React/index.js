d01d74c1aeb26c9ddbbeb3ae8fa5baf9b7a18256
MohammedHaq <mohammedzainhaq@gmail.com>
2022-10-24 10:54:39 -0400

    more practice

---
+++ React/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))   // this function takes two parameters: ReactDOM.render(what do I want to render, where do I want to render it)
+
+
+
+
